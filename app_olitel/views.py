e4d7008b960277bf651b78b1da700cfff4a1249d
mmirandah <mateo@correo.com>
2023-05-01 13:22:13 -0500

    cambiando nombres para identificar si funcion de django

--- app_olitel/views.py
+++ app_olitel/views.py
@@ -9,10 +9,10 @@ from django.http import HttpResponse
 def index(request):
   return HttpResponse("Hello Geeks")
 
-from .forms import InputForm
+from .forms import InputForm_ejemplo
  
 # Create your views here.
 def formulario_nombre(request):
     context ={}
-    context['form']= InputForm()
+    context['form']= InputForm_ejemplo()
     return render(request, "home_2.html", context)
