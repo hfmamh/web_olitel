5282dfb12f1f65639403ffc55d31dac446f24f06
mmirandah <mateo@correo.com>
2023-05-01 13:58:12 -0500

    con widgets

--- app_olitel/forms.py
+++ app_olitel/forms.py
@@ -11,7 +11,7 @@ class InputForm_ejemplo(forms.Form):
     password = forms.CharField(widget = forms.PasswordInput())
 
 class WidgetForm_ejemplo(forms.Form):
-    title = forms.CharField()
-    description = forms.CharField()
-    views = forms.IntegerField()
-    available = forms.BooleanField()
+    title = forms.CharField(widget = forms.Textarea)
+    description = forms.CharField(widget = forms.CheckboxInput)
+    views = forms.IntegerField(widget = forms.TextInput)
+    available = forms.BooleanField(widget = forms.Textarea)
