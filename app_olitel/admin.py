b0dd4114b46ebcde3fb2b156e645d7a90b225c6e
mmirandah <mateo@correo.com>
2023-05-15 09:57:52 -0500

    agregado a la vistad de admin productos

--- app_olitel/admin.py
+++ app_olitel/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import modelo_1
+from .models import modelo_1,productos
 admin.site.register(modelo_1)
+admin.site.register(productos)
