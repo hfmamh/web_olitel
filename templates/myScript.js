e6bf4f4ea58d90edc20cf30f284d707d2a74803c
mmirandah <mateo@correo.com>
2023-05-16 23:50:34 -0500

    fase 1  casi lista

---
+++ templates/myScript.js
@@ -0,0 +1,13 @@
+var counter = 0
+function sumar(){
+    counter = counter+1;
+    document.getElementById("demo3").innerHTML = counter;
+}
+function restar(){
+    counter = counter-1;
+    document.getElementById("demo3").innerHTML = counter;
+}
+function duplicar(){
+    counter = counter*2;
+    document.getElementById("demo3").innerHTML = counter;
+}
